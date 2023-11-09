338d9940641f0e8a115c07e14ca3b377b8a3d1d0
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2023-11-09 13:07:36 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nick Krasnitskiy on 09.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
